1abd46933eee744460b1c7b1a7b06b507302289e
Negar <negarsaljooghi@gmail.com>
2024-05-30 12:04:20 +0330

    adding explicit type

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,15 @@
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi';
+age = 30;
+//isLoggedIn = 25;
+isLoggedIn = true;
 // arrays
-var names = ['luigi', 'mario', 'yoshi'];
-// names = 'hello';
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[1] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-// objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking'];
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
-};
+var ninjas = [];
+// ninjas = [10, 23];
+ninjas = ['yoshi', 'mario'];
+ninjas.push('shaun');
+// union types
+//objects
--- sandbox.ts
+++ sandbox.ts
@@ -1,38 +1,23 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi';
+age = 30;
+
+//isLoggedIn = 25;
+isLoggedIn = true;
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let ninjas: string[] = [];
 
-// names = 'hello';
+// ninjas = [10, 23];
+ninjas = ['yoshi', 'mario'];
+ninjas.push('shaun');
 
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
+// union types
 
-let numbers = [10, 20, 30, 40];
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[1] = 'shaun';
+//objects
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-
-// objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking'];
-
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40
-}
