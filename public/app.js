cbb32ab645dbc682d5d2197adfa3178285a0924b
Negar <negarsaljooghi@gmail.com>
2024-06-06 19:04:20 +0330

    add classes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,27 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// // if (anchor) {
-// //     console.log(anchor.href);
-// // }
-// console.log(anchor.href);
-//const form = document.querySelector('form')!;
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes$${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 200);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let Invoices = [];
+// Invoices.push('hello');
+// Invoices.push({name:'shaun'});
+Invoices.push(invOne);
+Invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+// invTwo.amount = 'hello';
+console.log(invOne, invTwo);
 const form = document.querySelector('.new-item-form');
-//console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,38 @@
-// const anchor = document.querySelector('a')!;
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: Number;
 
-// // if (anchor) {
-// //     console.log(anchor.href);
-// // }
+    constructor(c: string, d: string, a: Number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes$${this.amount} for ${this.details}`
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 200);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-// console.log(anchor.href);
+let Invoices: Invoice[] = [];
 
-//const form = document.querySelector('form')!;
+// Invoices.push('hello');
+// Invoices.push({name:'shaun'});
+
+Invoices.push(invOne);
+Invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+// invTwo.amount = 'hello';
+
+console.log(invOne, invTwo);
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-//console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
